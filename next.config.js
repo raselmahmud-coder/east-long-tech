1222e0d04a1ce6aa06cdd9bf52e1024a9060ca27
raselmahmud22 <raselmahmud454b@gmail.com>
2024-03-21 03:38:19 +0800

    next config comments all

--- next.config.js
+++ next.config.js
@@ -1,32 +1,39 @@
-const nextConfig = {
+module.exports = {
   images: {
     unoptimized: true,
   },
-  output: "export",
-  distDir: "dist",
-  // Other Next.js configuration options can be added here
+}
 
-  webpack: (config, options) => {
-    const { isServer } = options;
-    const prefix = config.assetPrefix ?? config.basePath ?? "";
-    config.module.rules.push({
-      test: /\.mp4$/,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            publicPath: `${prefix}/_next/static/media/`,
-            outputPath: `${isServer ? "../" : ""}static/media/`,
-            name: "[name].[hash].[ext]",
-          },
-        },
-      ],
-    });
 
-    // Apply additional custom webpack configurations here
+// const nextConfig = {
+//   images: {
+//     unoptimized: true,
+//   },
+//   output: "export",
+//   distDir: "dist",
+//   // Other Next.js configuration options can be added here
 
-    return config; // Important: Return the modified config
-  },
-};
+//   webpack: (config, options) => {
+//     const { isServer } = options;
+//     const prefix = config.assetPrefix ?? config.basePath ?? "";
+//     config.module.rules.push({
+//       test: /\.mp4$/,
+//       use: [
+//         {
+//           loader: "file-loader",
+//           options: {
+//             publicPath: `${prefix}/_next/static/media/`,
+//             outputPath: `${isServer ? "../" : ""}static/media/`,
+//             name: "[name].[hash].[ext]",
+//           },
+//         },
+//       ],
+//     });
 
-module.exports = nextConfig;
+//     // Apply additional custom webpack configurations here
+
+//     return config; // Important: Return the modified config
+//   },
+// };
+
+// module.exports = nextConfig;
